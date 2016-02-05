8a33a024f6ee3c77b386b806d1b2c3e96b501552
Timbar21 <timbar21pb@gmail.com>
2016-02-04 19:29:14 -0500

    First commit

---
+++ instagramlab/PhotosViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramlab
+//
+//  Created by Tim Barnard on 2/4/16.
+//  Copyright (c) 2016 Tim Barnard. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
